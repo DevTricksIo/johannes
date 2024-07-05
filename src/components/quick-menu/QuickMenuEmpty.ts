9cb8d229bfe272f2989dce31b1a02265a4812965
Alexandre <alexandretavares@outlook.com>
2024-07-05 10:49:42 -0300

    Created a QuickMenuEmpty component

---
+++ src/components/quick-menu/QuickMenuEmpty.ts
@@ -0,0 +1,27 @@
+import BaseUIComponent from '../common/BaseUIComponent';
+
+class QuickMenuEmpty extends BaseUIComponent {
+
+    display: string;
+
+    constructor() {
+
+        super();
+
+        this.display = 'block';
+    }
+
+    init(): HTMLElement {
+
+        const htmlEmptyIndicator = document.createElement('span');
+        htmlEmptyIndicator.innerText = 'No results';
+        htmlEmptyIndicator.classList.add('empty-block-options');
+        htmlEmptyIndicator.style.padding = '10px';
+        htmlEmptyIndicator.style.color = 'rgba(55, 53, 47, 0.65)';
+        htmlEmptyIndicator.style.display = 'none';
+
+        return htmlEmptyIndicator;
+    }
+}
+
+export default QuickMenuEmpty;
