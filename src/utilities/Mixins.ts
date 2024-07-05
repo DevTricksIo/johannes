f8456c506662419eb38c733871c6161c68cea32e
Alexandre <alexandretavares@outlook.com>
2024-07-05 15:50:01 -0300

    Created a Mixins to simulate multiple inheritance

---
+++ src/utilities/Mixins.ts
@@ -0,0 +1,15 @@
+class Mixins {
+
+    private constructor() { };
+
+    static applyMixins(derivedCtor: any, baseCtors: any[]) {
+        baseCtors.forEach(baseCtor => {
+            Object.getOwnPropertyNames(baseCtor.prototype).forEach(name => {
+                Object.defineProperty(derivedCtor.prototype, name,
+                    Object.getOwnPropertyDescriptor(baseCtor.prototype, name) || Object.create(null));
+            });
+        });
+    }
+}
+
+export default Mixins;
