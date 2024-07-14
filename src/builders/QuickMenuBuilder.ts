6ba4824fcab87b0aa0b8d5ba5e3e0be160cbf154
Alexandre <alexandretavares@outlook.com>
2024-07-14 17:12:13 -0300

    Used const instead magic variable

--- src/builders/QuickMenuBuilder.ts
+++ src/builders/QuickMenuBuilder.ts
@@ -2,6 +2,7 @@ import QuickMenu from "../components/quick-menu/QuickMenu";
 import QuickMenuSection from "../components/quick-menu/QuickMenuSection";
 import QuickMenuItem from "../components/quick-menu/QuickMenuItem";
 import IBlockOperationsService from "../services/block-operations/IBlockOperationsService";
+import { ELEMENT_TYPES } from "../services/element-factory/ElementFactoryService";
 
 class QuickMenuBuilder {
 
@@ -12,14 +13,14 @@ class QuickMenuBuilder {
         const basicBlocksSection: QuickMenuSection = new QuickMenuSection(quickMenu, 'Basic blocks', 'basic-section');
 
         basicBlocksSection.appendQuickMenuItems([
-            new QuickMenuItem(basicBlocksSection, 'Paragraph', 'Just start writing with plain text.', 'icon-wordpress-paragraph', 'p'),
+            new QuickMenuItem(basicBlocksSection, 'Paragraph', 'Just start writing with plain text.', 'icon-wordpress-paragraph', ELEMENT_TYPES.PARAGRAPH),
             new QuickMenuItem(basicBlocksSection, 'Image', 'Just start writing with plain text.', 'icon-wordpress-paragraph', 'image'),
             new QuickMenuItem(basicBlocksSection, 'Bulleted list', 'Organize items with bullet points.', 'icon-wordpress-bulleted-list', 'bulleted-list'),
             new QuickMenuItem(basicBlocksSection, 'Numbered list', 'List items in a numbered format.', 'icon-wordpress-numbered-list', 'numbered-list'),
             new QuickMenuItem(basicBlocksSection, 'Code', 'Insert code snippets with syntax highlighting.', 'icon-wordpress-code-mark', 'code'),
             new QuickMenuItem(basicBlocksSection, 'Quote', 'Highlight text as a significant quote.', 'icon-wordpress-quote', 'quote'),
-            new QuickMenuItem(basicBlocksSection, 'Heading 2', 'Medium header for subsections.', 'icon-julia-head-2', 'h2'),
-            new QuickMenuItem(basicBlocksSection, 'Heading 3', 'Small header for detailed sections.', 'icon-julia-head-2', 'h3'),
+            new QuickMenuItem(basicBlocksSection, 'Heading 2', 'Medium header for subsections.', 'icon-julia-head-2', ELEMENT_TYPES.HEADER_2),
+            new QuickMenuItem(basicBlocksSection, 'Heading 3', 'Small header for detailed sections.', 'icon-julia-head-2', ELEMENT_TYPES.HEADER_3),
             new QuickMenuItem(basicBlocksSection, 'Separator', 'Visually divide blocks.', 'icon-wordpress-separator', 'separator')
         ]);
 
@@ -28,12 +29,12 @@ class QuickMenuBuilder {
         const headingBlocksSection = new QuickMenuSection(quickMenu, 'Heading', 'heading-section');
 
         headingBlocksSection.appendQuickMenuItems([
-            new QuickMenuItem(headingBlocksSection, 'Heading 1', 'Large header to organize content.', 'icon-julia-head-1', 'h1'),
-            new QuickMenuItem(headingBlocksSection, 'Heading 2', 'Medium header for subsections.', 'icon-julia-head-2', 'h2'),
-            new QuickMenuItem(headingBlocksSection, 'Heading 3', 'Small header for detailed sections.', 'icon-julia-head-3', 'h3'),
-            new QuickMenuItem(headingBlocksSection, 'Heading 4', 'Small header for detailed sections.', 'icon-julia-head-4', 'h4'),
-            new QuickMenuItem(headingBlocksSection, 'Heading 5', 'Small header for detailed sections.', 'icon-julia-head-5', 'h5'),
-            new QuickMenuItem(headingBlocksSection, 'Heading 6', 'Small header for detailed sections.', 'icon-julia-head-6', 'h6'),
+            new QuickMenuItem(headingBlocksSection, 'Heading 1', 'Large header to organize content.', 'icon-julia-head-1', ELEMENT_TYPES.BLOCK_HEADER_1),
+            new QuickMenuItem(headingBlocksSection, 'Heading 2', 'Medium header for subsections.', 'icon-julia-head-2', ELEMENT_TYPES.HEADER_2),
+            new QuickMenuItem(headingBlocksSection, 'Heading 3', 'Small header for detailed sections.', 'icon-julia-head-3', ELEMENT_TYPES.HEADER_3),
+            new QuickMenuItem(headingBlocksSection, 'Heading 4', 'Small header for detailed sections.', 'icon-julia-head-4', ELEMENT_TYPES.HEADER_4),
+            new QuickMenuItem(headingBlocksSection, 'Heading 5', 'Small header for detailed sections.', 'icon-julia-head-5', ELEMENT_TYPES.HEADER_5),
+            new QuickMenuItem(headingBlocksSection, 'Heading 6', 'Small header for detailed sections.', 'icon-julia-head-6', ELEMENT_TYPES.HEADER_6),
         ]);
 
         quickMenu.append(headingBlocksSection);
@@ -48,7 +49,7 @@ class QuickMenuBuilder {
         ]);
 
         quickMenu.append(listBlocksSection);
-        
+
 
         return quickMenu;
     }
