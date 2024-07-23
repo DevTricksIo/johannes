f8f61bd9d23aa4dc9f0d2fbb7071c174fbe36511
Alexandre <alexandretavares@outlook.com>
2024-07-23 09:56:04 -0300

    Created the functionality of deleteAndFocusOnPrevious and deleteAndFocusOnNext

--- src/services/block-operations/BlockOperationsService.ts
+++ src/services/block-operations/BlockOperationsService.ts
@@ -9,7 +9,9 @@ export class BlockOperationsService implements IBlockOperationsService {
 
     static BLOCK_OPERATIONS = {
         TURN_INTO: "turnInto",
-        CREATE_DEFAULT_BLOCK: "CreateDefaultBlock"
+        CREATE_DEFAULT_BLOCK: "CreateDefaultBlock",
+        DELETE_FOCUS_ON_PREVIOUS: "DeleteAndFocusOnPrevious",
+        DELETE_FOCUS_ON_NEXT: "DeleteAndFocusOnNext"
     };
 
     private constructor(elementFactoryService: IElementFactoryService) {
@@ -19,6 +21,7 @@ export class BlockOperationsService implements IBlockOperationsService {
         }
 
         this.elementFactoryService = elementFactoryService;
+
         BlockOperationsService.instance = this;
     }
 
@@ -26,6 +29,20 @@ export class BlockOperationsService implements IBlockOperationsService {
 
         if (command == BlockOperationsService.BLOCK_OPERATIONS.CREATE_DEFAULT_BLOCK) {
             this.createDefaultBlock(null);
+
+            return true;
+        }
+
+        if (command == BlockOperationsService.BLOCK_OPERATIONS.DELETE_FOCUS_ON_PREVIOUS) {
+            this.deleteAndFocusOnPrevious();
+
+            return true;
+        }
+
+        if (command == BlockOperationsService.BLOCK_OPERATIONS.DELETE_FOCUS_ON_NEXT) {
+            this.deleteAndFocusOnNext();
+
+            return true;
         }
 
         if (command == BlockOperationsService.BLOCK_OPERATIONS.TURN_INTO) {
@@ -240,6 +257,304 @@ export class BlockOperationsService implements IBlockOperationsService {
         // focusable.focus();
 
         // focusOnTheEndOfTheText(focusable);
+    }
+
+
+    private deleteAndFocusOnPrevious(): void {
+
+        const currentActiveElement = document.activeElement!;
+
+        this.focusOnPrevious(currentActiveElement);
+        this.deleteTheCurrentElementAndTheDraggableBlockIfEmpty(currentActiveElement);
+    }
+
+    private deleteAndFocusOnNext() {
+
+        const currentActiveElement = document.activeElement;
+
+        if (!currentActiveElement) {
+            return;
+        }
+
+        this.focusOnNext(currentActiveElement);
+        this.deleteTheCurrentElementAndTheDraggableBlockIfEmpty(currentActiveElement);
+    }
+
+    focusOnPrevious(actualElement: Element, position: number | null = null): void {
+
+        let tag = actualElement.tagName.toUpperCase();
+        let focusedElement = null;
+
+        if (tag === 'LI') {
+            let previousElement = actualElement.previousElementSibling;
+
+            if (!previousElement) {
+                return;
+            }
+
+            if (previousElement && previousElement.classList.contains('focusable')) {
+                focusedElement = previousElement as HTMLElement;
+                if (position) {
+                    this.applyCursorXEndPosition(focusedElement, position);
+                } else {
+                    this.focusOnTheEndOfTheText(focusedElement);
+                }
+                // return focusedElement;
+                return;
+            }
+        }
+
+        if ((actualElement.parentNode as HTMLElement).tagName.toUpperCase() === 'LI' /* focusable SPAN inside LI*/) {
+
+            let previousElement = actualElement.closest('li')?.previousElementSibling?.querySelector('.focusable');
+
+            if (previousElement && previousElement.classList.contains('focusable')) {
+                focusedElement = previousElement as HTMLElement;
+                if (position) {
+                    this.applyCursorXEndPosition(focusedElement, position);
+                } else {
+                    this.focusOnTheEndOfTheText(focusedElement);
+                }
+                // return focusedElement;
+                return;
+            }
+        }
+
+        let parent = actualElement.closest('.block');
+
+        if (!parent) {
+            return;
+        }
+        let sibling = parent.previousElementSibling;
+
+        while (sibling) {
+            let focusableCandidates = sibling.querySelectorAll('.focusable');
+            if (focusableCandidates.length > 0) {
+                focusedElement = focusableCandidates[focusableCandidates.length - 1] as HTMLElement;
+                if (position) {
+                    this.applyCursorXEndPosition(focusedElement, position);
+                } else {
+                    this.focusOnTheEndOfTheText(focusedElement);
+                }
+                // return focusedElement;
+                return;
+            }
+
+            sibling = sibling.previousElementSibling;
+        }
+
+        // return focusedElement;
+        return;
+    }
+
+    focusOnNext(actualElement: Element, position: number | null = null) {
+        let tag = actualElement.tagName.toUpperCase();
+        let focusedElement = null;
+
+        if (tag === 'LI') {
+            let nextElement = actualElement.nextElementSibling;
+
+            if (nextElement && nextElement.classList.contains('focusable')) {
+                focusedElement = nextElement as HTMLElement;
+
+                if (position) {
+                    this.applyCursorXStartPosition(focusedElement, position);
+                } else {
+                    this.focusOnTheStartOfTheText(focusedElement);
+                }
+                return focusedElement;
+            }
+        }
+
+        if ((actualElement.parentNode as HTMLElement).tagName.toUpperCase() === 'LI' /* focusable SPAN inside LI*/) {
+            let nextElement = actualElement.closest('li')?.nextElementSibling?.querySelector('.focusable');
+
+            if (nextElement && nextElement.classList.contains('focusable')) {
+                focusedElement = nextElement as HTMLElement;
+                if (position) {
+                    this.applyCursorXStartPosition(focusedElement, position);
+                } else {
+                    this.focusOnTheStartOfTheText(focusedElement);
+                }
+                return focusedElement;
+            }
+        }
+
+        let parent = actualElement.closest('.block');
+
+        if (!parent) {
+            return;
+        }
+
+        let sibling = parent.nextElementSibling;
+
+        while (sibling) {
+            let focusableCandidates = sibling.querySelectorAll('.focusable');
+            if (focusableCandidates.length > 0) {
+                focusedElement = focusableCandidates[0] as HTMLElement;
+                if (position) {
+                    this.applyCursorXStartPosition(focusedElement, position);
+                } else {
+                    this.focusOnTheStartOfTheText(focusedElement);
+                }
+                return focusedElement;
+            }
+
+            sibling = sibling.nextElementSibling;
+        }
+
+        return focusedElement;
+    }
+
+
+
+    deleteTheCurrentElementAndTheDraggableBlockIfEmpty(currentElement: Element) {
+
+        const parentBlock = currentElement.closest('.block');
+        const actual = currentElement.closest('.deletable');
+
+        actual?.remove();
+
+        if (parentBlock && parentBlock.querySelectorAll('.editable').length == 0) {
+            parentBlock.remove();
+        }
+    }
+
+
+
+
+    applyCursorXEndPosition(element: HTMLElement, xPosition: number) {
+
+        const selection = window.getSelection()!;
+        const range = document.createRange();
+
+        let currentNode = element.lastChild;
+        let result = null;
+
+        while (currentNode) {
+            if (currentNode.nodeType === Node.TEXT_NODE) {
+                result = this.adjustCursorOffset(currentNode, xPosition);
+                break;
+            } else if (currentNode.nodeName.toUpperCase() === 'BR') {
+                currentNode = currentNode.previousSibling;
+                continue;
+            }
+            currentNode = currentNode.previousSibling;
+        }
+
+        if (result && result.closestNode) {
+            range.setStart(result.closestNode, result.closestOffset);
+            range.collapse(true);
+            selection.removeAllRanges();
+            selection.addRange(range);
+            element.focus();
+        } else {
+            range.selectNodeContents(element);
+            range.collapse(false);
+            selection.removeAllRanges();
+            selection.addRange(range);
+            element.focus();
+        }
+    }
+
+    applyCursorXStartPosition(element: HTMLElement, xPosition: number) {
+        const selection = window.getSelection()!;
+        const range = document.createRange();
+
+        let currentNode = element.firstChild;
+        let result = null;
+
+        while (currentNode) {
+            if (currentNode.nodeType === Node.TEXT_NODE) {
+                result = this.adjustCursorOffset(currentNode, xPosition);
+                break;
+            }
+            currentNode = currentNode.nextSibling;
+        }
+
+        if (result && result.closestNode) {
+            range.setStart(result.closestNode, result.closestOffset);
+            range.collapse(true);
+            selection.removeAllRanges();
+            selection.addRange(range);
+            element.focus();
+        } else {
+            range.selectNodeContents(element);
+            range.collapse(true);
+            selection.removeAllRanges();
+            selection.addRange(range);
+            element.focus();
+        }
+    }
+
+    adjustCursorOffset(node: Node, xPosition: number) {
+        let range = document.createRange();
+        let closestNode = node;
+        let closestOffset = 0;
+        let closestDiff = Infinity;
+
+        for (let i = 0; i < node.textContent!.length; i++) {
+            range.setStart(node, i);
+            range.setEnd(node, i + 1);
+            const rect = range.getBoundingClientRect();
+            const leftDiff = Math.abs(rect.left - xPosition);
+            const rightDiff = Math.abs(rect.right - xPosition);
+
+            if (leftDiff < closestDiff || rightDiff < closestDiff) {
+                closestDiff = Math.min(leftDiff, rightDiff);
+                closestOffset = i + (rightDiff < leftDiff ? 1 : 0);
+            }
+        }
+
+        if (xPosition > range.getBoundingClientRect().right) {
+            closestOffset = node.textContent!.length;
+        }
+
+        return { closestNode, closestOffset };
+    }
+
+
+    focusOnTheEndOfTheText(contentBlock: HTMLElement) {
+
+        setTimeout(() => {
+
+            const range = document.createRange();
+            const selection = window.getSelection()!;
+
+            range.selectNodeContents(contentBlock);
+
+            let lastChild = contentBlock;
+
+            while (lastChild.lastChild && lastChild.lastChild.nodeType === Node.ELEMENT_NODE) {
+                lastChild = lastChild.lastChild as HTMLElement;
+            }
+            if (lastChild.lastChild) {
+                lastChild = lastChild.lastChild as HTMLElement;
+            }
+
+            range.setEnd(lastChild, lastChild.textContent!.length);
+            range.collapse(false);
+
+            selection.removeAllRanges();
+            selection.addRange(range);
+
+            contentBlock.focus();
+        }, 10);
+    }
+
+    focusOnTheStartOfTheText(contentBlock: HTMLElement) {
+
+        setTimeout(() => {
+            const range = document.createRange();
+            const selection = window.getSelection()!;
+
+            range.selectNodeContents(contentBlock);
+            range.collapse(true);
+            selection.removeAllRanges();
+            selection.addRange(range);
+
+            contentBlock.focus();
+        }, 10);
     }
 
 }
