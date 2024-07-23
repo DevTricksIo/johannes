a910f06cb84934c1ef5dddb071cdac8d228475b2
Alexandre <alexandretavares@outlook.com>
2024-07-23 09:51:33 -0300

    Updated the AddBlockButton test

--- src/components/add-block/AddBlockButton.test.ts
+++ src/components/add-block/AddBlockButton.test.ts
@@ -6,12 +6,9 @@ describe("AddBlockButton", () => {
     beforeEach(() => {
         // Setup the editor in the document body
         document.body.innerHTML = `
-            <div id="johannesEditor">
-                <div class="content">
-                    <p class="editable">Initial paragraph</p>
-                </div>
-            </div>
-        `;
+        <div id="johannesEditor"></div>
+    `;
+
         const editor = UIBuilder.build().start();
         expect(editor).toBeInstanceOf(Editor);
     });
@@ -29,3 +26,66 @@ describe("AddBlockButton", () => {
         expect(paragraphs.length).toBe(2);
     });
 });
+
+
+
+
+
+
+
+
+
+
+
+
+// import { UIBuilder } from "../../builders/UIBuilder";
+// import { Editor } from "../editor/Editor";
+
+// describe("AddBlockButton", () => {
+
+//     beforeEach(() => {
+//         // Setup the editor in the document body
+//         document.body.innerHTML = `
+//         <div id="johannesEditor"></div>
+//         <script>
+//             const editorConfig = {
+//                 enableFloatingToolbar: true,
+//                 enableQuickMenu: true,
+//                 enableAddBlock: true,
+//                 includeHeader: true,
+//                 includeFirstParagraph: true,
+//                 enableTitle: true,
+//                 title: undefined
+//             };
+//             window.editorConfig = editorConfig;
+//         </script>
+//     `;
+
+//         const editorConfig = {
+//             enableFloatingToolbar: true,
+//             enableQuickMenu: true,
+//             enableAddBlock: true,
+//             includeHeader: true,
+//             includeFirstParagraph: true,
+//             enableTitle: true,
+//             title: undefined
+//         };
+//         window.editorConfig = editorConfig;
+
+//         const editor = UIBuilder.build().start();
+//         expect(editor).toBeInstanceOf(Editor);
+//     });
+
+//     test("Add functionality", () => {
+
+//         let paragraphs = document.querySelectorAll("#johannesEditor .content p");
+//         expect(paragraphs.length).toBe(1);
+
+//         const addBlockButton = document.querySelector(".add-block") as HTMLButtonElement;
+
+//         addBlockButton.click();
+
+//         paragraphs = document.querySelectorAll("#johannesEditor .content p");
+//         expect(paragraphs.length).toBe(2);
+//     });
+// });
