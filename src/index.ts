3361838986781ce95365f92d1ad78bf51d271770
Alexandre <alexandretavares@outlook.com>
2024-07-30 20:05:04 -0300

    Index is now responsible to start the app

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,17 @@
-// listen events in keyboard, mouse and document load
-import './triggers/load-events.js';
-import './triggers/keypress-events.js';
-import './triggers/click-events.js';
-
-import './block-operation.js';
-import './drag-and-drop.js';
-import './switch-block.js';
-import './keyboard-navigation.js';
-import './text-blocks-from-newlines.js';
-import './memento.js';
-import './assets/img/icons.svg';
-
-
-// default style
 import './assets/css/main.css';
+import { UIBuilder } from './builders/UIBuilder';
+import { CommandDispatcher } from './commands/CommandDispatcher';
+
+
+/**
+ * This script initializes the editor and sets up event listeners once the DOM content has fully loaded.
+ * It is responsible for building the user interface and starting the event listening process to handle commands.
+ *
+ * The `DOMContentLoaded` event ensures that all the DOM content has been fully parsed and loaded before the initialization begins.
+ * `UIBuilder.build().start();` is used to construct and activate the user interface.
+ * `CommandDispatcher.getInstance().listen();` retrieves an instance of the CommandDispatcher and starts listening for events.
+ */
+document.addEventListener('DOMContentLoaded', function () {
+    UIBuilder.build().start();
+    CommandDispatcher.getInstance().listen();
+});
