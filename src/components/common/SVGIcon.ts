088895de337c3c1b57be4512171532ccfb422df7
Alexandre <alexandretavares@outlook.com>
2024-08-09 12:29:18 -0300

    Updated the SVGIcon component

--- src/components/common/SVGIcon.ts
+++ src/components/common/SVGIcon.ts
@@ -1,18 +1,14 @@
-// import BaseUIComponent from "./BaseUIComponent";
-
 import { BaseUIComponent } from "./BaseUIComponent";
 
 export class SVGIcon extends BaseUIComponent {
 
     useElement: SVGUseElement;
 
-    constructor(hrefUseId: string, width = "1rem", height = "1rem") {
+    constructor(iconId: string, size: string) {
 
         super({
-            hrefUseId: hrefUseId,
-            width: width,
-            height: height,
-
+            iconId: iconId,
+            size: size
         })
 
         this.useElement = this.htmlElement.querySelector('use') as SVGUseElement;
@@ -22,24 +18,31 @@ export class SVGIcon extends BaseUIComponent {
 
         const htmlElement = document.createElementNS("http://www.w3.org/2000/svg", "svg");
 
-        htmlElement.style.color = "rgba(55, 53, 47, 0.85)";
+        // htmlElement.style.color = "rgba(55, 53, 47, 0.85)";
+        htmlElement.style.color = "red !important";
 
         let use = document.createElementNS("http://www.w3.org/2000/svg", "use");
-        use.setAttributeNS("http://www.w3.org/1999/xlink", "href", `#${this.props.hrefUseId}`);
+        use.setAttributeNS("http://www.w3.org/1999/xlink", "href", `#${this.props.iconId}`);
+
+        use.style.color = "red !important";
 
         htmlElement.appendChild(use);
-        htmlElement.setAttribute('width', this.props.width);
-        htmlElement.setAttribute('height', this.props.height);
+        htmlElement.setAttribute('width', this.props.size);
+        htmlElement.setAttribute('height', this.props.size);
         htmlElement.setAttribute('fill', 'currentColor');
 
         return htmlElement as unknown as HTMLElement;
     }
 
     clone(): SVGIcon {
-        return new SVGIcon(this.props.hrefUseId, this.props.width, this.props.height);
+        return new SVGIcon(this.props.iconId, this.props.sizes);
     }
 
     setUseTo(value: string): void {
         this.useElement.setAttributeNS("http://www.w3.org/1999/xlink", "href", `#${value}`);
+    }
+
+    static create(iconId: string, size: string): SVGIcon {
+        return new SVGIcon(iconId, size);
     }
 }
