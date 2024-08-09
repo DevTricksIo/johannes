c71bac8945d02b550d874f06a1b539b35425e9a4
Alexandre <alexandretavares@outlook.com>
2024-08-09 12:30:01 -0300

    Updated de Content component

--- src/components/content/Content.test.ts
+++ src/components/content/Content.test.ts
@@ -1,14 +1,9 @@
 
-import { ElementFactoryService } from "@/services/element-factory/ElementFactoryService";
 import { Content } from "./Content";
-import { BlockOperationsService } from "@/services/block-operations/BlockOperationsService";
 
 describe("Content", () => {
     test("Create element", () => {
-        const elementFactoryService = ElementFactoryService.getInstance();
-        const blockOperationsService = BlockOperationsService.getInstance();
-
-        const content = new Content(elementFactoryService, blockOperationsService);
+        const content = Content.getInstance();
 
         expect(content).toBeInstanceOf(Content);
     });
--- src/components/content/Content.ts
+++ src/components/content/Content.ts
@@ -1,24 +1,18 @@
-import { IElementFactoryService } from "@/services/element-factory/IElementFactoryService";
 import { ElementFactoryService } from "@/services/element-factory/ElementFactoryService";
 import { BaseUIComponent } from "../common/BaseUIComponent";
-import { IBlockOperationsService } from "@/services/block-operations/IBlockOperationsService";
-import { BlockOperationsService } from "@/services/block-operations/BlockOperationsService";
-import { QuickMenu } from "../quick-menu/QuickMenu";
+import { CustomEvents } from "@/common/CustomEvents";
+import { Commands } from "@/commands/Commands";
+import { ICommandEventDetail } from "@/commands/ICommandEventDetail";
+import { DependencyContainer } from "@/core/DependencyContainer";
+import { IQuickMenu } from "../quick-menu/IQuickMenu";
+import { IShortcutListeners } from "@/core/IShortcutListeners";
+import { ITableListeners } from "@/core/listeners/ITableListeners";
+import { ITableContextFloatingToolbar } from "../floating-toolbar/ITableContextFloatingToolbar";
 
 export class Content extends BaseUIComponent {
+    constructor() {
 
-    blockOperationsService: IBlockOperationsService;
-
-    constructor(
-        elementFactoryService: IElementFactoryService,
-        blockOperationsService: IBlockOperationsService) {
-
-        super({
-            elementFactoryService: elementFactoryService,
-            blockOperationsService: blockOperationsService
-        });
-
-        this.blockOperationsService = blockOperationsService;
+        super({});
 
         this.attachEvent();
     }
@@ -29,21 +23,47 @@ export class Content extends BaseUIComponent {
         htmlElement.classList.add("content");
 
         if (window.editorConfig?.includeFirstParagraph || true) {
-
-            const blockParagraph = this.props.elementFactoryService.create(ElementFactoryService.ELEMENT_TYPES.BLOCK_PARAGRAPH);
-
-            htmlElement.append(blockParagraph);
+            htmlElement.append(ElementFactoryService.blockParagraph());
         }
 
         return htmlElement;
     }
 
+    /**
+    * Clears text selection when initiating a drag from elements with the `.drag-handler` class.
+    * This function attaches a `mousedown` event listener to the entire document. When a mousedown
+    * event occurs on an element that is a child of a `.drag-handler` or on the `.drag-handler` itself,
+    * it checks if the target or its parent is a designated drag handler. If true, it clears any current text selections.
+    * This prevents text from being accidentally selected during drag-and-drop interactions, enhancing UX in draggable interfaces.
+    * 
+    * @function clearSelectionOnDrag
+    */
+    clearSelectionOnDrag() {
+        document.addEventListener("mousedown", (event) => {
+            const element = event.target as HTMLElement;
+            let parent: Element | null;
+
+            if (element) {
+                if (element.nodeType == Node.TEXT_NODE) {
+                    parent = element.parentElement;
+                } else {
+                    parent = element;
+                }
+
+                if (parent && parent.closest(".drag-handler")) {
+                    window.getSelection()?.removeAllRanges();
+                }
+            }
+        });
+    }
+
     attachEvent(): void {
 
+        this.clearSelectionOnDrag();
+        this.reRenderPlaceholder();
+
         document.addEventListener("copiedText", () => {
-
             const copyElementItem = document.querySelector("#copyOption .text-option span") as HTMLSpanElement;
-
             if (copyElementItem) {
 
                 copyElementItem.textContent = "Copied!";
@@ -54,102 +74,59 @@ export class Content extends BaseUIComponent {
             }
         });
 
-        //Shortcuts
-        document.addEventListener("keydown", (event) => {
+        //Focus on P when load
+        document.addEventListener('DOMContentLoaded', function () {
+            const editor = document.querySelector('.johannes-editor');
 
+            if (editor) {
+                let blocks = editor.querySelectorAll('.block');
 
-            console.log(`Key: ${event.key}, Code: ${event.code}, Ctrl: ${event.ctrlKey}, Alt: ${event.altKey}, AltGr: ${event.key === 'AltGraph'}, NumLock: ${event.getModifierState('NumLock')}`);
+                if (blocks.length == 1) {
 
-            const isNumPad = event.code.startsWith("Numpad");
-            const numLockOn = event.getModifierState("NumLock");
-
-            if ((event.ctrlKey || event.metaKey) && event.shiftKey && (event.key === "Enter")) {
-                // Convert to check list
-                event.preventDefault();
-                event.stopPropagation();
-                this.blockOperationsService.execCommand(BlockOperationsService.BLOCK_OPERATIONS.TRANSFORM_BLOCK, false, ElementFactoryService.ELEMENT_TYPES.PARAGRAPH);
-            } else if ((event.ctrlKey || event.metaKey) && !event.shiftKey && (event.code === "Digit1" || event.code === "Numpad1")) {
-                // Convert to check list
-                event.preventDefault();
-                event.stopPropagation();
-                this.blockOperationsService.execCommand(BlockOperationsService.BLOCK_OPERATIONS.TRANSFORM_BLOCK, false, ElementFactoryService.ELEMENT_TYPES.CHECK_LIST);
-            } else if ((event.ctrlKey || event.metaKey) && !event.shiftKey && event.key === ".") {
-                // Convert to bulleted
-                event.preventDefault();
-                event.stopPropagation();
-                this.blockOperationsService.execCommand(BlockOperationsService.BLOCK_OPERATIONS.TRANSFORM_BLOCK, false, ElementFactoryService.ELEMENT_TYPES.BULLETED_LIST);
-            } else if ((event.ctrlKey || event.metaKey) && !event.shiftKey && (event.key === "/")) {
-                // Convert to numbered
-                event.preventDefault();
-                event.stopPropagation();
-                this.blockOperationsService.execCommand(BlockOperationsService.BLOCK_OPERATIONS.TRANSFORM_BLOCK, false, ElementFactoryService.ELEMENT_TYPES.NUMBERED_LIST);
-            } else if ((event.ctrlKey && event.altKey && ((event.code === "Digit1") || (isNumPad && numLockOn && event.code === "Numpad1")))) {
-                // Convert to h1
-                event.preventDefault();
-                event.stopPropagation();
-                this.blockOperationsService.execCommand(BlockOperationsService.BLOCK_OPERATIONS.TRANSFORM_BLOCK, false, ElementFactoryService.ELEMENT_TYPES.HEADER_1);
-            }
-            else if ((event.ctrlKey && event.altKey && ((event.code === "Digit2") || (isNumPad && numLockOn && event.code === "Numpad2")))) {
-                // Convert to h2
-                event.preventDefault();
-                event.stopPropagation();
-                this.blockOperationsService.execCommand(BlockOperationsService.BLOCK_OPERATIONS.TRANSFORM_BLOCK, false, ElementFactoryService.ELEMENT_TYPES.HEADER_2);
-            } else if ((event.ctrlKey && event.altKey && ((event.code === "Digit3") || (isNumPad && numLockOn && event.code === "Numpad3")))) {
-                // Convert to h3
-                event.preventDefault();
-                event.stopPropagation();
-                this.blockOperationsService.execCommand(BlockOperationsService.BLOCK_OPERATIONS.TRANSFORM_BLOCK, false, ElementFactoryService.ELEMENT_TYPES.HEADER_3);
-            } else if ((event.ctrlKey && event.altKey && ((event.code === "Digit4") || (isNumPad && numLockOn && event.code === "Numpad4")))) {
-                // Convert to h4
-                event.preventDefault();
-                event.stopPropagation();
-                this.blockOperationsService.execCommand(BlockOperationsService.BLOCK_OPERATIONS.TRANSFORM_BLOCK, false, ElementFactoryService.ELEMENT_TYPES.HEADER_4);
-            } else if ((event.ctrlKey && event.altKey && ((event.code === "Digit5") || (isNumPad && numLockOn && event.code === "Numpad5")))) {
-                // Convert to h5
-                event.preventDefault();
-                event.stopPropagation();
-                this.blockOperationsService.execCommand(BlockOperationsService.BLOCK_OPERATIONS.TRANSFORM_BLOCK, false, ElementFactoryService.ELEMENT_TYPES.HEADER_5);
-            } else if ((event.ctrlKey && event.altKey && ((event.code === "Digit6") || (isNumPad && numLockOn && event.code === "Numpad6")))) {
-                // Convert to h6
-                event.preventDefault();
-                event.stopPropagation();
-                this.blockOperationsService.execCommand(BlockOperationsService.BLOCK_OPERATIONS.TRANSFORM_BLOCK, false, ElementFactoryService.ELEMENT_TYPES.HEADER_6);
-            } else if ((event.key === "D" || event.key === "d") && event.ctrlKey) {
-                // Duplicate block shortcut
-                event.preventDefault();
-                event.stopPropagation();
-                this.blockOperationsService.execCommand(BlockOperationsService.BLOCK_OPERATIONS.DUPLICATE, false);
-            } else if ((event.key === "Delete" || event.key === "Backspace") && event.shiftKey) {
-                // Delete block shortcut
-                event.preventDefault();
-                event.stopPropagation();
-                this.blockOperationsService.execCommand(BlockOperationsService.BLOCK_OPERATIONS.DELETE, false);
-            } else if (event.key === "\\" && (event.ctrlKey || event.metaKey)) {
-                // Clear formatting
-                this.blockOperationsService.execCommand(BlockOperationsService.BLOCK_OPERATIONS.REMOVE_FORMAT, false);
-            } else if (event.ctrlKey && event.altKey && event.code === "Digit1") {
-                // Prevent character insertion when using top row '1'
-                event.preventDefault();
+                    const p = blocks[0].querySelector('.johannes-content-element') as HTMLElement;
+                    if (p.innerText == '') {
+                        p.focus();
+                    }
+                }
             }
         });
 
 
+
+
         this.htmlElement.addEventListener("keydown", async (event) => {
+
+            const quickMenu = DependencyContainer.Instance.resolve<IQuickMenu>("IQuickMenu");
+            const tableToolbar = DependencyContainer.Instance.resolve<ITableContextFloatingToolbar>("ITableContextFloatingToolbar");
 
             if (event.ctrlKey || event.shiftKey || event.altKey) {
                 return;
             }
 
-
-            if (event.key === 'Enter' && !QuickMenu.getInstance().isVisible) {
+            if (event.key === 'Enter' && !quickMenu.isVisible && !tableToolbar.isVisible) {
+                // Create a default block when press Enter
                 event.preventDefault();
-
-                this.blockOperationsService.execCommand(BlockOperationsService.BLOCK_OPERATIONS.CREATE_DEFAULT_BLOCK, false);
+                document.dispatchEvent(new CustomEvent<ICommandEventDetail>(CustomEvents.emittedCommand, {
+                    detail: {
+                        command: Commands.insertNew,
+                    }
+                }));
             } else if (event.key === 'Backspace') {
                 const target = event.target as HTMLElement;
 
                 if (target.classList.contains('johannes-content-element') && target.textContent?.trim() === '') {
-                    this.blockOperationsService.execCommand(BlockOperationsService.BLOCK_OPERATIONS.DELETE_FOCUS_ON_PREVIOUS, false);
+
+                    document.dispatchEvent(new CustomEvent<ICommandEventDetail>(CustomEvents.emittedCommand, {
+                        detail: {
+                            command: Commands.focusOnPreviousBlock,
+                        }
+                    }));
+
+                    document.dispatchEvent(new CustomEvent<ICommandEventDetail>(CustomEvents.emittedCommand, {
+                        detail: {
+                            command: Commands.focusOnPreviousBlock,
+                        }
+                    }));
                 } else if (target.classList.contains('johannes-content-element') && target.textContent?.trim() !== '') {
                     //TODO: Replace the delete and focus by merge
                 }
@@ -158,23 +135,58 @@ export class Content extends BaseUIComponent {
                 const target = event.target as HTMLElement;
 
                 if (target.classList.contains('johannes-content-element') && target.textContent?.trim() === '') {
-                    this.blockOperationsService.execCommand(BlockOperationsService.BLOCK_OPERATIONS.DELETE_FOCUS_ON_NEXT, false);
+                    // this.blockOperationsService.execCommand(BlockOperationsService.BLOCK_OPERATIONS.DELETE_FOCUS_ON_NEXT, false);
                 } else if (target.classList.contains('johannes-content-element') && target.textContent?.trim() !== '') {
                     //TODO: Replace the delete and focus by merge
                 }
             } else if (event.key === 'ArrowRight' && Content.isCursorAtEnd(event.target as HTMLElement)) {
-                this.blockOperationsService.execCommand(BlockOperationsService.BLOCK_OPERATIONS.FOCUS_ON_NEXT, false);
+                // this.blockOperationsService.execCommand(BlockOperationsService.BLOCK_OPERATIONS.FOCUS_ON_NEXT, false);
             } else if (event.key === 'ArrowLeft' && Content.isCursorAtStart(event.target as HTMLElement)) {
-                this.blockOperationsService.execCommand(BlockOperationsService.BLOCK_OPERATIONS.FOCUS_ON_PREVIOUS, false);
+                // this.blockOperationsService.execCommand(BlockOperationsService.BLOCK_OPERATIONS.FOCUS_ON_PREVIOUS, false);
             } else if (event.key === 'ArrowDown' && Content.isCursorOnLastLine()) {
-                this.blockOperationsService.execCommand(BlockOperationsService.BLOCK_OPERATIONS.FOCUS_ON_NEXT, false);
+                // this.blockOperationsService.execCommand(BlockOperationsService.BLOCK_OPERATIONS.FOCUS_ON_NEXT, false);
             } else if (event.key === 'ArrowUp' && Content.isCursorOnFirstLine()) {
-                this.blockOperationsService.execCommand(BlockOperationsService.BLOCK_OPERATIONS.FOCUS_ON_PREVIOUS, false);
+                // this.blockOperationsService.execCommand(BlockOperationsService.BLOCK_OPERATIONS.FOCUS_ON_PREVIOUS, false);
             }
         });
     }
 
 
+
+    /**
+    * Adds an input event listener to the entire document to handle placeholder behavior for contentEditable elements.
+    * This workaround is specifically designed to address a known issue in Firefox where contentEditable elements
+    * do not properly reset their placeholders after the content is deleted by the user.
+    * 
+    * The event listener checks if the target of the input event is a contentEditable element and whether it has a
+    * custom 'data-placeholder' attribute. If the element's content is empty (ignoring white spaces), the function
+    * resets the 'data-placeholder' to ensure it displays correctly, and clears any residual text content that might
+    * interfere with the placeholder display.
+    * 
+    * @example
+    * // To utilize this workaround, ensure your contentEditable elements have a 'data-placeholder' attribute.
+    * // <div contentEditable="true" data-placeholder="Enter text here..."></div>
+    * 
+    * @param {Event} event - The input event triggered by user interaction with the document's input-capable elements.
+    */
+    reRenderPlaceholder() {
+        document.addEventListener('input', function (event: Event) {
+            if (event.target instanceof HTMLElement) {
+                const editableElement = event.target;
+
+                if (editableElement.isContentEditable) {
+                    if (editableElement.hasAttribute('data-placeholder')) {
+                        const customPlaceholder = editableElement.getAttribute('data-placeholder');
+
+                        if (editableElement.textContent?.trim() === '') {
+                            editableElement.setAttribute('data-placeholder', customPlaceholder || '');
+                            editableElement.textContent = '';
+                        }
+                    }
+                }
+            }
+        });
+    }
 
     static isCursorAtEnd(target: HTMLElement): boolean {
         const focusableParent = target.closest('.focusable');
@@ -253,8 +265,6 @@ export class Content extends BaseUIComponent {
         });
     }
 
-
-
     static isCursorOnFirstLine(): boolean {
         const selection = window.getSelection();
         if (!selection || !selection.rangeCount) return false;
@@ -271,5 +281,12 @@ export class Content extends BaseUIComponent {
         const range = selection.getRangeAt(0);
 
         return range.endOffset === range.endContainer.textContent?.length && range.endContainer === range.commonAncestorContainer;
+    }
+
+    static getInstance(): Content {
+
+        const shortcutListener = DependencyContainer.Instance.resolve<IShortcutListeners>("IShortcutListeners");
+        const tableListeners = DependencyContainer.Instance.resolve<ITableListeners>("ITableListeners");
+        return new Content();
     }
 }
