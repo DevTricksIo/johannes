41c616a97bc28be5064da478bb274306b39120a3
Alexandre <alexandretavares@outlook.com>
2024-08-26 00:02:28 -0300

    Restore the navigation

--- src/components/content/Content.ts
+++ src/components/content/Content.ts
@@ -8,6 +8,10 @@ import { IQuickMenu } from "../quick-menu/IQuickMenu";
 import { IShortcutListeners } from "@/core/IShortcutListeners";
 import { ITableListeners } from "@/core/listeners/ITableListeners";
 import { ITableContextFloatingToolbar } from "../floating-toolbar/ITableContextFloatingToolbar";
+import { DOMUtils } from "@/utilities/DOMUtils";
+import { TableUtils } from "@/utilities/TableUtils";
+import { DefaultJSEvents } from "@/common/DefaultJSEvents";
+import { KeyboardKeys } from "@/common/KeyboardKeys";
 
 export class Content extends BaseUIComponent {
     constructor() {
@@ -39,7 +43,7 @@ export class Content extends BaseUIComponent {
     * @function clearSelectionOnDrag
     */
     clearSelectionOnDrag() {
-        document.addEventListener("mousedown", (event) => {
+        document.addEventListener(DefaultJSEvents.Mousedown, (event) => {
             const element = event.target as HTMLElement;
             let parent: Element | null;
 
@@ -57,10 +61,63 @@ export class Content extends BaseUIComponent {
         });
     }
 
+
     attachEvent(): void {
 
         this.clearSelectionOnDrag();
         this.reRenderPlaceholder();
+
+
+        // Event selector
+        document.addEventListener(DefaultJSEvents.Click, (event: MouseEvent) => {
+            // Remove a classe 'block-selected' de qualquer bloco previamente selecionado
+            const previousSelected = document.querySelectorAll('.separator-selected');
+
+            previousSelected.forEach(selected => {
+                selected.classList.remove('separator-selected');
+                selected.removeAttribute('tabindex'); // Remove tabindex quando não está mais selecionado
+            });
+
+            // Verifica se o elemento clicado é um filho de um 'separator'
+            let currentElement = event.target as HTMLElement; // Cast do target para HTMLElement
+
+            if (currentElement && currentElement.closest(".separator-wrapper")) {
+
+                event.stopImmediatePropagation();
+                event.preventDefault();
+
+                let parentBlock = currentElement.closest('.separator-wrapper') as HTMLElement; // Cast para HTMLElement
+                if (parentBlock) {
+                    parentBlock.classList.add('separator-selected');
+                    parentBlock.setAttribute('tabindex', '-1'); // Adiciona tabindex para permitir foco
+
+                    setTimeout(() => {
+                        parentBlock.focus(); // Coloca o foco no bloco selecionado
+                    }, 50);
+                }
+            }
+
+        });
+
+        document.addEventListener(DefaultJSEvents.Blur, (event: FocusEvent) => {
+            const target = event.target as Node;
+
+            if (target instanceof HTMLElement && target.closest('.separator-selected')) {
+
+                event.stopImmediatePropagation();
+                const separator = target.closest('.separator-selected')!;
+
+                separator.classList.remove('separator-selected');
+                separator.removeAttribute('tabindex'); // Limpa o atributo para não interferir com a navegação normal
+
+                // Reaplicar atributos que garantem o foco
+                // setTimeout(() => {
+                //     separator.setAttribute('tabindex', '-1');
+                // }, 50);
+            }
+        }, true);
+        //end event selector
+
 
         document.addEventListener("copiedText", () => {
             const copyElementItem = document.querySelector("#copyOption .text-option span") as HTMLSpanElement;
@@ -75,7 +132,7 @@ export class Content extends BaseUIComponent {
         });
 
         //Focus on P when load
-        document.addEventListener('DOMContentLoaded', function () {
+        document.addEventListener(DefaultJSEvents.DOMContentLoaded, function () {
             const editor = document.querySelector('.johannes-editor');
 
             if (editor) {
@@ -92,9 +149,18 @@ export class Content extends BaseUIComponent {
         });
 
 
+        document.addEventListener(DefaultJSEvents.Keydown, (event: KeyboardEvent) => {
+            if (event.key === 'Enter' && !event.shiftKey && DOMUtils.isEventTargetDescendantOf(event, "#johannesEditor .content-wrapper .title")) {
+                event.preventDefault();
+                document.dispatchEvent(new CustomEvent(CustomEvents.pressedEnterOnTitle, {}));
+            }
+        }, true);
 
+        document.addEventListener(DefaultJSEvents.Keydown, async (event) => {
 
-        this.htmlElement.addEventListener("keydown", async (event) => {
+            if (DOMUtils.isEventTargetDescendantOf(event, ".ignore-events") && event.key !== 'Tab') {
+                return;
+            }
 
             const quickMenu = DependencyContainer.Instance.resolve<IQuickMenu>("IQuickMenu");
             const tableToolbar = DependencyContainer.Instance.resolve<ITableContextFloatingToolbar>("ITableContextFloatingToolbar");
@@ -103,41 +169,127 @@ export class Content extends BaseUIComponent {
                 return;
             }
 
-            if (event.key === 'Enter' && !quickMenu.isVisible && !tableToolbar.isVisible) {
+
+            if (event.key === KeyboardKeys.Enter && !event.shiftKey && !quickMenu.isVisible && !tableToolbar.isVisible) {
+
+                // if(DOMUtils.findClosestAncestorOfActiveElementByClass(".johannes-code")){
+                //     event.stopImmediatePropagation();
+                //     return;
+                // }
+
+                event.preventDefault();
+
+                const tableController = (event.target as Element).closest(".table-controller");
+                if (tableController) {
+                    const activeCell = (event.target as Element).closest("td, th") as HTMLTableCellElement;
+                    const table = tableController.querySelector("table") as HTMLTableElement;
+                    if (activeCell) {
+
+                        const focusedBelow = TableUtils.moveFocusToBelowCell(table, activeCell);
+                        if (!focusedBelow) {
+
+                            document.dispatchEvent(new CustomEvent<ICommandEventDetail>(CustomEvents.emittedCommand, {
+                                detail: {
+                                    command: Commands.focusOnNextBlock,
+                                }
+                            }));
+                        }
+                    }
+
+                    return;
+                }
+
+
+                if ((event.target as Element).closest(".johannes-code")) {
+                    return;
+                }
+
                 // Create a default block when press Enter
                 event.preventDefault();
+                event.stopImmediatePropagation();
+
                 document.dispatchEvent(new CustomEvent<ICommandEventDetail>(CustomEvents.emittedCommand, {
                     detail: {
                         command: Commands.insertNew,
                     }
                 }));
-            } else if (event.key === 'Backspace') {
+
+            } else if (event.key === KeyboardKeys.Backspace) {
                 const target = event.target as HTMLElement;
 
-                if (target.classList.contains('johannes-content-element') && target.textContent?.trim() === '') {
+                if (target.classList.contains('separator-wrapper')) {
+
+                    event.stopImmediatePropagation();
 
                     document.dispatchEvent(new CustomEvent<ICommandEventDetail>(CustomEvents.emittedCommand, {
                         detail: {
-                            command: Commands.focusOnPreviousBlock,
+                            command: Commands.deleteBlockAndFocusOnPrevious,
                         }
                     }));
+                } else if (target.closest(".johannes-content-element") && target.textContent?.trim() === '') {
+
+                    event.stopImmediatePropagation();
 
                     document.dispatchEvent(new CustomEvent<ICommandEventDetail>(CustomEvents.emittedCommand, {
                         detail: {
-                            command: Commands.focusOnPreviousBlock,
+                            command: Commands.deleteBlockAndFocusOnPrevious,
                         }
                     }));
-                } else if (target.classList.contains('johannes-content-element') && target.textContent?.trim() !== '') {
-                    //TODO: Replace the delete and focus by merge
+
+                } else if (target.closest('.johannes-content-element') && target.textContent?.trim() !== '') {
+
+                    const { atStart, atEnd } = DOMUtils.getSelectionTextInfo(target);
+                    const selectedContent = document.getSelection()?.toString();
+
+                    if (atStart && selectedContent?.length == 0) {
+
+                        event.preventDefault();
+                        event.stopImmediatePropagation();
+
+                        document.dispatchEvent(new CustomEvent<ICommandEventDetail>(CustomEvents.emittedCommand, {
+                            detail: {
+                                command: Commands.mergeWithPreviousBlock,
+                            }
+                        }));
+                    }
                 }
 
-            } else if (event.key === 'Delete') {
+            } else if (event.key === KeyboardKeys.Delete) {
                 const target = event.target as HTMLElement;
 
-                if (target.classList.contains('johannes-content-element') && target.textContent?.trim() === '') {
-                    // this.blockOperationsService.execCommand(BlockOperationsService.BLOCK_OPERATIONS.DELETE_FOCUS_ON_NEXT, false);
-                } else if (target.classList.contains('johannes-content-element') && target.textContent?.trim() !== '') {
-                    //TODO: Replace the delete and focus by merge
+                if (target.classList.contains('separator-wrapper')) {
+
+                    event.stopImmediatePropagation();
+
+                    document.dispatchEvent(new CustomEvent<ICommandEventDetail>(CustomEvents.emittedCommand, {
+                        detail: {
+                            command: Commands.deleteBlockAndFocusOnNext,
+                        }
+                    }));
+                } else if (target.classList.contains('johannes-content-element') && target.textContent?.trim() === '') {
+                    event.stopImmediatePropagation();
+
+                    document.dispatchEvent(new CustomEvent<ICommandEventDetail>(CustomEvents.emittedCommand, {
+                        detail: {
+                            command: Commands.deleteBlockAndFocusOnNext,
+                        }
+                    }));
+                } else if (target.closest('.johannes-content-element') && target.textContent?.trim() !== '') {
+
+                    const { atStart, atEnd } = DOMUtils.getSelectionTextInfo(target);
+                    const selectedContent = document.getSelection()?.toString();
+
+                    if (atEnd && selectedContent?.length == 0) {
+
+                        event.preventDefault();
+                        event.stopImmediatePropagation();
+
+                        document.dispatchEvent(new CustomEvent<ICommandEventDetail>(CustomEvents.emittedCommand, {
+                            detail: {
+                                command: Commands.mergeWithNextBlock,
+                            }
+                        }));
+                    }
                 }
             } else if (event.key === 'ArrowRight' && Content.isCursorAtEnd(event.target as HTMLElement)) {
                 // this.blockOperationsService.execCommand(BlockOperationsService.BLOCK_OPERATIONS.FOCUS_ON_NEXT, false);
@@ -147,10 +299,30 @@ export class Content extends BaseUIComponent {
                 // this.blockOperationsService.execCommand(BlockOperationsService.BLOCK_OPERATIONS.FOCUS_ON_NEXT, false);
             } else if (event.key === 'ArrowUp' && Content.isCursorOnFirstLine()) {
                 // this.blockOperationsService.execCommand(BlockOperationsService.BLOCK_OPERATIONS.FOCUS_ON_PREVIOUS, false);
+            } else if (event.key === 'Tab') {
+
+                if (DOMUtils.isEventTargetDescendantOf(event, ".johannes-code")) {
+                    event.preventDefault();
+                    const tabCharacter = '\u00a0\u00a0\u00a0\u00a0';  // Representa 4 espaços (ou você pode usar um verdadeiro caractere de tabulação '\t' se preferir)
+                    document.execCommand('insertText', false, tabCharacter);
+                }
             }
         });
+
+
+        document.addEventListener(DefaultJSEvents.Blur, (event: Event) => this.sanitizeElementEventHandler(event), true)
     }
 
+
+    sanitizeElementEventHandler(event: Event): void {
+        const target = event.target as HTMLElement;
+
+        // Verifica se o elemento alvo é contenteditable
+        if (target instanceof HTMLElement && target.contentEditable === "true") {
+            console.log("lipou");
+            DOMUtils.sanitizeContentEditable(target);
+        }
+    }
 
 
     /**
@@ -170,7 +342,7 @@ export class Content extends BaseUIComponent {
     * @param {Event} event - The input event triggered by user interaction with the document's input-capable elements.
     */
     reRenderPlaceholder() {
-        document.addEventListener('input', function (event: Event) {
+        document.addEventListener(DefaultJSEvents.Input, function (event: Event) {
             if (event.target instanceof HTMLElement) {
                 const editableElement = event.target;
 
