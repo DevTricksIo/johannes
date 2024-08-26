c2143fd35ca76194e450de8bf86e99514dabf297
Alexandre <alexandretavares@outlook.com>
2024-08-25 23:45:06 -0300

    Added new keys to KeyboardKeys enum

--- src/common/KeyboardKeys.ts
+++ src/common/KeyboardKeys.ts
@@ -1,5 +1,9 @@
 export enum KeyboardKeys {
     Shift = "Shift",
     Enter = "Enter",
-    Escape = "Escape"
+    Escape = "Escape",
+    Backspace = "Backspace",
+    Control = "Control",
+    Space = " ",
+    Delete = "Delete"
 }
