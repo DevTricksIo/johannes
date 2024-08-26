34772d38b8772cd5a7a77e2bd4da6c9bbc2eaa0d
Alexandre <alexandretavares@outlook.com>
2024-08-25 23:55:31 -0300

    Created new element types

--- src/services/element-factory/ElementFactoryService.ts
+++ src/services/element-factory/ElementFactoryService.ts
@@ -1,6 +1,9 @@
 import { ContentTypes } from "@/common/ContentTypes";
 import { IElementFactoryService } from "./IElementFactoryService";
 import { Utils } from "@/utilities/Utils";
+import { Icons } from "@/common/Icons";
+import { ToolboxOptions } from "@/components/block-toolbox/ToolboxOptions";
+import { CommonClasses } from "@/common/CommonClasses";
 
 interface ElementCreator {
     (content: string | null): HTMLElement;
@@ -30,6 +33,14 @@ export class ElementFactoryService implements IElementFactoryService {
         HEADER_6: "h6",
         DRAG_HANDLE_BUTTON: "drag-handle-button",
         TABLE: "table",
+        IMAGE: "image",
+        VIDEO: "video",
+        SPOTIFY: "spotify",
+        GITHUB_GIST: "github-gist",
+        GITLAB_SNIPPET: "gitlab-snippet",
+        CODEPEN: "codepen",
+        CALLOUT: "callout",
+        SEPARATOR: "separator"
     }
 
     private constructor() {
@@ -57,7 +68,14 @@ export class ElementFactoryService implements IElementFactoryService {
         this.register(ElementFactoryService.ELEMENT_TYPES.HEADER_6, ElementFactoryService.headingCreator(6));
         this.register(ElementFactoryService.ELEMENT_TYPES.DRAG_HANDLE_BUTTON, ElementFactoryService.dragHandleButtonCreator());
         this.register(ElementFactoryService.ELEMENT_TYPES.TABLE, ElementFactoryService.tableCreator());
-
+        this.register(ElementFactoryService.ELEMENT_TYPES.IMAGE, ElementFactoryService.placeholderCreator(ContentTypes.Image, Icons.Image2, "Upload or embed an image", [CommonClasses.ShowMediaInputUpload, CommonClasses.ShowMediaInputEmbed, "image-embed-placeholder-text"]));
+        this.register(ElementFactoryService.ELEMENT_TYPES.VIDEO, ElementFactoryService.placeholderCreator(ContentTypes.Iframe, Icons.YouTube, "Embed a YouTube video", [CommonClasses.ShowMediaInputEmbed, "youtube-embed-placeholder-text"]));
+        this.register(ElementFactoryService.ELEMENT_TYPES.SPOTIFY, ElementFactoryService.placeholderCreator(ContentTypes.Iframe, Icons.Spotify, "Embed a Spotify audio track", [CommonClasses.ShowMediaInputEmbed, "spotify-embed-placeholder-text"]));
+        this.register(ElementFactoryService.ELEMENT_TYPES.GITHUB_GIST, ElementFactoryService.placeholderCreator(ContentTypes.Iframe, Icons.GitHub, "Embed a GitHub Gist", [CommonClasses.ShowMediaInputEmbed, "github-gist-embed-placeholder-text"]));
+        //this.register(ElementFactoryService.ELEMENT_TYPES.GITLAB_SNIPPET, ElementFactoryService.placeholderCreator(ContentTypes.Script, Icons.GitLab, "Embed a snippet", [CommonClasses.ShowMediaInputEmbed]));
+        this.register(ElementFactoryService.ELEMENT_TYPES.CODEPEN, ElementFactoryService.placeholderCreator(ContentTypes.Iframe, Icons.CodePen, "Embed a CodePen web demo", [CommonClasses.ShowMediaInputEmbed, "codepen-embed-placeholder-text"]));
+        this.register(ElementFactoryService.ELEMENT_TYPES.CALLOUT, ElementFactoryService.calloutCreator());
+        this.register(ElementFactoryService.ELEMENT_TYPES.SEPARATOR, ElementFactoryService.separatorCreator());
 
 
         ElementFactoryService._instance = this;
@@ -126,27 +144,73 @@ export class ElementFactoryService implements IElementFactoryService {
 
     private static codeCreator(): ElementCreator {
         return content => {
+            const container = document.createElement('div');
+            container.classList.add("johannes-content-element", "ignore-events");
+
+            const codeBlock = document.createElement("div");
+            codeBlock.classList.add("code-block", "ignore-quick-menu", "ignore-text-floating-toolbar");
+
             const pre = document.createElement('pre');
+            pre.classList.add(ToolboxOptions.IncludeBlockToolbarClass, ToolboxOptions.LanguageSelectionToolClass, ToolboxOptions.ExtraOptionsClass);
+
             const code = document.createElement('code');
+            code.contentEditable = "true";
+            code.setAttribute("data-placeholder", "/* Code snippet */");
             code.textContent = content || "";
+            code.classList.add('johannes-code', "focusable", "hljs", "language-plaintext");
+            code.setAttribute("spellCheck", "false");
+
             pre.appendChild(code);
 
-            code.contentEditable = "true";
-            pre.classList.add('johannes-content-element');
-            code.classList.add('johannes-code');
+            code.addEventListener("blur", () => {
+                code.removeAttribute("data-highlighted");
+                hljs.highlightElement(code);
+            });
 
-            return pre;
+            // codeBlock.appendChild(languageSelector);
+            codeBlock.appendChild(pre);
+            container.appendChild(codeBlock);
+
+            hljs.highlightElement(code);
+
+            return container;
         };
     }
 
     private static quoteCreator(): ElementCreator {
         return content => {
-            const blockquote = document.createElement('blockquote');
+
+            const contentElement = document.createElement("div");
+            contentElement.classList.add("johannes-content-element", "swittable");
+
+            const blockquote = document.createElement("blockquote");
+            blockquote.classList.add("focusable");
             blockquote.textContent = content || "";
             blockquote.contentEditable = "true";
-            blockquote.classList.add('johannes-content-element');
-            return blockquote;
+            blockquote.setAttribute("data-placeholder", ElementFactoryService.getRandomQuote());
+
+            contentElement.appendChild(blockquote);
+            
+            return contentElement;
         };
+    }
+
+    static getRandomQuote(): string {
+        const quotations = [
+            "Talk is cheap. Show me the code. - Linus Torvalds",
+            "Premature optimization is the root of all evil. - Donald Knuth",
+            "The most dangerous phrase in the language is, 'We've always done it this way.' - Grace Hopper",
+            "Simplicity is prerequisite for reliability. - Edsger W. Dijkstra",
+            "Any fool can write code that a computer can understand. Good programmers write code that humans can understand. - Martin Fowler",
+            "First, solve the problem. Then, write the code. - John Johnson",
+            "In programming, the hard part isn't solving problems, but deciding what problems to solve. - Paul Graham",
+            "Code is like humor. When you have to explain it, it's bad. - Cory House",
+            "Make it work, make it right, make it fast. - Kent Beck",
+            "Before software can be reusable it first has to be usable. - Ralph Johnson"
+        ];
+    
+        const index = Math.floor(Math.random() * quotations.length);    
+        return quotations[index];
     }
 
     private static checkListCreator(): ElementCreator {
@@ -204,7 +268,7 @@ export class ElementFactoryService implements IElementFactoryService {
         return content => {
 
             const tableShell = document.createElement("div");
-            tableShell.classList.add("table-shell");
+            tableShell.classList.add("table-shell", "ignore-quick-menu");
 
             const tableController = document.createElement("div");
             tableController.classList.add("table-controller");
@@ -213,7 +277,7 @@ export class ElementFactoryService implements IElementFactoryService {
             tableWrapper.classList.add("table-wrapper");
 
             const table = document.createElement('table');
-            table.classList.add('johannes-content-element', 'swittable', 'table');
+            table.classList.add("johannes-content-element", "swittable", "table", ToolboxOptions.IncludeBlockToolbarClass, ToolboxOptions.AlignToolClass, ToolboxOptions.ExtraOptionsClass);
             table.setAttribute("data-content-type", ContentTypes.Table);
 
             tableShell.appendChild(tableController);
@@ -243,7 +307,7 @@ export class ElementFactoryService implements IElementFactoryService {
             const addColumn = document.createElement("div");
             addColumn.classList.add("add-table-column");
             addColumn.innerHTML = `<svg width="1rem" height="1rem" fill="currentColor"><use xlink:href="#icon-plus"></use></svg>`;
-    
+
             tableController.appendChild(addRow);
             tableController.appendChild(addColumn);
 
@@ -251,14 +315,66 @@ export class ElementFactoryService implements IElementFactoryService {
         };
     }
 
-    // private static numberedListCreator(): ElementCreator {
-    //     return () => {
-    //         const ol = document.createElement('ol');
-    //         ol.contentEditable = "true";
-    //         ol.classList.add('johannes-content-element');
-    //         return ol;
-    //     };
-    // }
+    private static placeholderCreator(contentType: ContentTypes, icon: Icons, text: string, classes: string[] = []): ElementCreator {
+        return content => {
+
+            const contentElement = document.createElement('div');
+            contentElement.classList.add(CommonClasses.ContentElement, "swittable", "no-selection", ToolboxOptions.IncludeBlockToolbarClass, ToolboxOptions.ExtraOptionsClass);
+            contentElement.setAttribute("data-content-type", contentType);
+
+            const placeholder = document.createElement('div');
+            placeholder.classList.add("content-placeholder", CommonClasses.ShowMediaInputOnClick, ...classes);
+
+            const placeholderIcon = this.createIcon(icon);
+
+            const placeholderText = document.createElement("span");
+            placeholderText.classList.add("no-selection");
+            placeholderText.innerText = text;
+
+            contentElement.appendChild(placeholder);
+            placeholder.appendChild(placeholderIcon);
+            placeholder.appendChild(placeholderText);
+
+            return contentElement;
+        };
+    }
+
+    private static calloutCreator(): ElementCreator {
+        return content => {
+            const johannesCallout = document.createElement('div');
+            johannesCallout.classList.add("ignore-quick-menu", "callout", "johannes-content-element", "swittable", ToolboxOptions.IncludeBlockToolbarClass, ToolboxOptions.ColorToolClass, ToolboxOptions.ExtraOptionsClass);
+
+            const calloutWrapper = document.createElement("div");
+            calloutWrapper.classList.add("callout-background-grey", "callout-wrapper");
+            
+            const textArea = document.createElement('p');
+            textArea.setAttribute("data-placeholder", "Type something...");
+            textArea.contentEditable = "true";
+            textArea.classList.add("callout-text", "editable", "focusable");
+            
+            calloutWrapper.appendChild(textArea);
+            johannesCallout.appendChild(calloutWrapper);
+
+            return johannesCallout;
+        };
+    }
+
+    private static separatorCreator(): ElementCreator {
+        return () => {
+            const content = document.createElement("div");
+            content.classList.add("johannes-content-element");
+
+            const wrapper = document.createElement("div");
+            wrapper.classList.add("separator-wrapper");
+
+            const separator = document.createElement('hr');
+            separator.classList.add('separator');
+            wrapper.appendChild(separator);
+            content.appendChild(wrapper);
+            
+            return content;
+        };
+    }
 
     static paragraph(content: string | null = null): HTMLElement {
         const p = document.createElement('p');
@@ -296,9 +412,7 @@ export class ElementFactoryService implements IElementFactoryService {
         const id = Utils.generateUniqueId();
 
         let element = document.createElement('li');
-        element.classList.add('deletable');
-        element.classList.add('list-item');
-        element.classList.add('list-item-checkable');
+        element.classList.add("deletable", "no-list-style", "list-item", "list-item-checkable");
 
         // initialItem.classList.add('key-trigger');
 
@@ -374,6 +488,7 @@ export class ElementFactoryService implements IElementFactoryService {
     static blockParagraph(content: string | null = null) {
 
         let newDiv = document.createElement('div');
+        newDiv.id = `b-${Utils.generateUniqueId()}`;
         let newElement = ElementFactoryService.paragraph(content);
 
         // let dragHandler = document.createElement('button');
@@ -393,14 +508,28 @@ export class ElementFactoryService implements IElementFactoryService {
 
     static dragHandleButton() {
 
+        const dragHandlerWrapper = document.createElement("div");
+        dragHandlerWrapper.classList.add("drag-handler-wrapper", CommonClasses.EditorOnly);
+
         let button = document.createElement('button');
         button.innerHTML = '<svg width="1.375rem" height="1.375rem" fill="currentColor"><use href="#icon-material-drag"></use></svg>';
 
-        button.classList.add('drag-handler');
-        button.classList.add('button-reset');
+        button.classList.add(CommonClasses.EditorOnly, "drag-handler", "button-reset");
         button.draggable = true;
 
-        return button;
+
+        dragHandlerWrapper.appendChild(button);
+
+        return dragHandlerWrapper;
+    }
+
+    static createIcon(iconId: string) {
+
+        let element = document.createElement('div');
+        element.classList.add("icon-wrapper");
+        element.innerHTML = `<svg width="1.375rem" height="1.375rem" fill="currentColor"><use href="#${iconId}"></use></svg>`;
+
+        return element;
     }
 
 }
