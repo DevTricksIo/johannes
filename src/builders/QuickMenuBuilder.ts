c29faab4fca9590f4612c18603674b6e18ad16ce
Alexandre <alexandretavares@outlook.com>
2024-08-25 23:37:07 -0300

    Added more QuickMenu Items

--- src/builders/QuickMenuBuilder.ts
+++ src/builders/QuickMenuBuilder.ts
@@ -2,6 +2,7 @@ import { QuickMenu } from "../components/quick-menu/QuickMenu";
 import { QuickMenuSection } from "../components/quick-menu/QuickMenuSection";
 import { QuickMenuItem } from "../components/quick-menu/QuickMenuItem";
 import { ElementFactoryService } from "../services/element-factory/ElementFactoryService";
+import { Icons } from "@/common/Icons";
 
 export class QuickMenuBuilder {
 
@@ -9,22 +10,31 @@ export class QuickMenuBuilder {
 
         const quickMenu: QuickMenu = QuickMenu.getInstance();
 
-        const basicBlocksSection: QuickMenuSection = new QuickMenuSection({ quickMenuInstance: quickMenu, title: 'Basic blocks', classList: 'basic-section' });
+        const basicBlocksSection: QuickMenuSection = new QuickMenuSection({ quickMenuInstance: quickMenu, title: 'Basic', classList: 'basic-section' });
 
         basicBlocksSection.appendQuickMenuItems([
             new QuickMenuItem(basicBlocksSection, 'Text', 'Start writing in plain text.', 'icon-material-format', ElementFactoryService.ELEMENT_TYPES.PARAGRAPH, "paragraph text p"),
-            new QuickMenuItem(basicBlocksSection, 'Image', 'Upload image.', 'icon-material-aperture', 'image', "image figure photo illustration picture "),
-            new QuickMenuItem(basicBlocksSection, 'Bulleted list', 'Organize items with bullet points.', 'icon-wordpress-bulleted-list', 'bulleted-list', "bulleted list unordered list ul"),
-            new QuickMenuItem(basicBlocksSection, 'Numbered list', 'List items in a numbered format.', 'icon-wordpress-numbered-list', 'numbered-list', "numbered list number list sequential list ol enumerated list ordered list"),
+            new QuickMenuItem(basicBlocksSection, 'Bulleted list', 'Organize items with bullet points.', 'icon-wordpress-bulleted-list', ElementFactoryService.ELEMENT_TYPES.BULLETED_LIST, "bulleted list unordered list ul"),
+            new QuickMenuItem(basicBlocksSection, 'Numbered list', 'List items in a numbered format.', 'icon-wordpress-numbered-list', ElementFactoryService.ELEMENT_TYPES.NUMBERED_LIST, "numbered list number list sequential list ol enumerated list ordered list"),
             new QuickMenuItem(basicBlocksSection, 'Table', 'Organize data in rows and columns.', 'icon-material-table', ElementFactoryService.ELEMENT_TYPES.TABLE, "table tr th"),
-            new QuickMenuItem(basicBlocksSection, 'Code', 'Include code snippet.', 'icon-wordpress-code-mark', 'code', "code script source markup"),
-            new QuickMenuItem(basicBlocksSection, 'Quote', 'Highlight text as a quote.', 'icon-wordpress-quote', 'quote', "quote blockquote citation quotation"),
+            new QuickMenuItem(basicBlocksSection, 'Callout', 'Emphasize key points with a callout box.', Icons.Callout, ElementFactoryService.ELEMENT_TYPES.CALLOUT, "callout note spotlight"),
+            new QuickMenuItem(basicBlocksSection, 'Code', 'Include a code snippet.', 'icon-wordpress-code-mark', ElementFactoryService.ELEMENT_TYPES.CODE, "code script source markup"),
+            new QuickMenuItem(basicBlocksSection, 'Quote', 'Highlight text as a quote.', 'icon-wordpress-quote', ElementFactoryService.ELEMENT_TYPES.QUOTE, "quote blockquote citation quotation cite"),
             new QuickMenuItem(basicBlocksSection, 'Heading 2', 'Medium header for sections.', 'icon-julia-head-2', ElementFactoryService.ELEMENT_TYPES.HEADER_2, "header 2 heading 2 h2"),
             new QuickMenuItem(basicBlocksSection, 'Heading 3', 'Small header for subsections.', 'icon-julia-head-2', ElementFactoryService.ELEMENT_TYPES.HEADER_3, "header 3 heading 3 h3"),
-            new QuickMenuItem(basicBlocksSection, 'Separator', 'Visually divide blocks.', 'icon-wordpress-separator', 'separator', "separator divider rule line")
+            new QuickMenuItem(basicBlocksSection, 'Separator', 'Visually divide blocks.', 'icon-wordpress-separator', ElementFactoryService.ELEMENT_TYPES.SEPARATOR, "separator divider rule line hr")
         ]);
 
         quickMenu.append(basicBlocksSection);
+
+        const mediaBlocksSection = new QuickMenuSection({ quickMenuInstance: quickMenu, title: 'Media', classList: 'media-section' });
+
+        mediaBlocksSection.appendQuickMenuItems([
+            new QuickMenuItem(mediaBlocksSection, 'Image', 'Upload or embed an image.', Icons.Image2, ElementFactoryService.ELEMENT_TYPES.IMAGE, "image figure photo illustration picture media upload"),
+            new QuickMenuItem(mediaBlocksSection, 'YouTube', 'Embed a YouTube video.', Icons.YouTube, 'video', "video embed youtube clip film movie tape")
+        ]);
+
+        quickMenu.append(mediaBlocksSection);
 
         const headingBlocksSection = new QuickMenuSection({ quickMenuInstance: quickMenu, title: 'Heading', classList: 'heading-section' });
 
@@ -43,12 +53,26 @@ export class QuickMenuBuilder {
         const listBlocksSection = new QuickMenuSection({ quickMenuInstance: quickMenu, title: 'List', classList: 'list-section' });
 
         listBlocksSection.appendQuickMenuItems([
-            new QuickMenuItem(listBlocksSection, 'Todo list', 'Track tasks with checkboxes.', 'icon-material-check-list-2', 'todo-list', "todo list task list checklist"),
-            new QuickMenuItem(listBlocksSection, 'Bulleted list', 'Organize items with bullet points.', 'icon-wordpress-bulleted-list', 'bulleted-list', "bulleted list unordered list ul"),
-            new QuickMenuItem(listBlocksSection, 'Numbered list', 'List items in a numbered format.', 'icon-wordpress-numbered-list', 'numbered-list', "numbered list number list sequential list ol enumerated list ordered list")
+            new QuickMenuItem(listBlocksSection, 'Todo list', 'Track tasks with checkboxes.', 'icon-material-check-list-2', ElementFactoryService.ELEMENT_TYPES.CHECK_LIST, "todo list task list checklist"),
+            new QuickMenuItem(listBlocksSection, 'Bulleted list', 'Organize items with bullet points.', 'icon-wordpress-bulleted-list', ElementFactoryService.ELEMENT_TYPES.BULLETED_LIST, "bulleted list unordered list ul"),
+            new QuickMenuItem(listBlocksSection, 'Numbered list', 'List items in a numbered format.', 'icon-wordpress-numbered-list', ElementFactoryService.ELEMENT_TYPES.NUMBERED_LIST, "numbered list number list sequential list ol enumerated list ordered list")
         ]);
 
         quickMenu.append(listBlocksSection);
+        
+        
+        const embedSection = new QuickMenuSection({ quickMenuInstance: quickMenu, title: 'Embed', classList: 'embed-section' });
+
+        embedSection.appendQuickMenuItems([
+            new QuickMenuItem(embedSection, 'GitHub Gist', 'Embed a GitHub Gist.', Icons.GitHub, 'github-gist', "code gist github snippet git"),
+            // new QuickMenuItem(embedSection, 'GitLab Snippet ', 'List items in a numbered format.', Icons.GitLab, 'gitlab-snippet', "code git gitlab snippet gist"),
+            // new QuickMenuItem(embedSection, 'GitLab Snippet ', 'List items in a numbered format.', 'icon-wordpress-numbered-list', 'numbered-list', "numbered list number list sequential list ol enumerated list ordered list"),
+            new QuickMenuItem(embedSection, 'CodePen', 'Embed a CodePen web demo.', Icons.CodePen, 'codepen', "code codepen"),
+            // new QuickMenuItem(embedSection, 'Google Maps', 'List items in a numbered format.', 'icon-wordpress-numbered-list', 'numbered-list', "numbered list number list sequential list ol enumerated list ordered list"),
+            new QuickMenuItem(embedSection, 'Spotify', 'Embed a Spotify audio track.', Icons.Spotify, 'spotify', "mp3 sound audio music spotify track"),
+        ]);
+
+        quickMenu.append(embedSection);
 
         return quickMenu;
     }
