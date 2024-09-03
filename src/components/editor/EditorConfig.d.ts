0b7a4af1cfc59299b1bd8c3afa919bffd8202530
Alexandre <alexandretavares@outlook.com>
2024-09-03 15:45:20 -0300

    Added thumbnail to EditorConfig

--- src/components/editor/EditorConfig.d.ts
+++ src/components/editor/EditorConfig.d.ts
@@ -5,6 +5,7 @@ export interface EditorConfig {
     includeHeader: boolean;
     includeFirstParagraph: boolean;
     enableTitle: boolean;
+    enableThumbnail: boolean;
     title: string | undefined
 }
 
